38a48d5886b48eee7cc1f1c605ec177621bbbc0c
Jon Goodger <jonno.is@gmail.com>
2019-06-16 00:30:49 +0100

    Skeleton for day 4

---
+++ aoc/d4.py
@@ -0,0 +1,14 @@
+def d4_1(lines):
+    pass
+
+
+def d4_2(lines):
+    pass
+
+
+if __name__ == '__main__':
+    with open('data/4.txt', 'r') as f:
+        lines = list(f)
+
+    print('Part 1:', d4_1(lines))
+    print('Part 2:', d4_2(lines))
