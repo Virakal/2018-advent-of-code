0a52883fa34e2f9a5aac8ebb1e9d4a82508a9d85
Jon Goodger <jonno.is@gmail.com>
2019-06-15 09:21:29 +0100

    Finish 3-2

--- aoc/d3.py
+++ aoc/d3.py
@@ -23,7 +23,39 @@ def d3_1(lines):
 
 
 def d3_2(lines):
-    return 'NYI'
+    claims = {}
+
+    # Build claims
+    for line in lines:
+        claim = Claim.from_string(line)
+        claims[claim.id] = claim.get_claimed_coords()
+
+    # Work out claims per coord
+    claimed_coords = {}
+
+    for id in claims:
+        coords_list = claims[id]
+
+        for coords in coords_list:
+            if coords in claimed_coords:
+                claimed_coords[coords].append(id)
+            else:
+                claimed_coords[coords] = [id]
+
+    # Find coords with only one claim
+    undisputed_coords = set(
+        coords for coords in claimed_coords if len(claimed_coords[coords]) == 1)
+
+    # Find a claim where every coord is undisputed
+    for id in claims:
+        correct = True
+        for coords in claims[id]:
+            if coords not in undisputed_coords:
+                correct = False
+                break
+
+        if correct:
+            return id
 
 
 if __name__ == '__main__':
--- tests/test_d3.py
+++ tests/test_d3.py
@@ -20,3 +20,17 @@ def test_d3_1_example_1():
     ]
 
     assert d3_1(lines) == 4
+
+
+def test_d3_2_answer(filedata):
+    assert d3_2(filedata) == 877
+
+
+def test_d3_2_example_1():
+    lines = [
+        '#1 @ 1,3: 4x4',
+        '#2 @ 3,1: 4x4',
+        '#3 @ 5,5: 2x2',
+    ]
+
+    assert d3_2(lines) == 3
